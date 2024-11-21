eb91143d75af21561d5006bce7f204d0c0cab448
SamRex398 <juwona71@gmail.com>
2024-11-21 09:55:36 +0100

    admin

--- simple-django-crud-app/myproject/api/admin.py
+++ simple-django-crud-app/myproject/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'user')  # Display these fields in the list view
+    search_fields = ('title', 'content')  # Allow searching by title or content
+
+# Register the Note model with the custom admin class
+admin.site.register(Note, NoteAdmin)
